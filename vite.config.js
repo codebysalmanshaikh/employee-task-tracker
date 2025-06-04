52e9f9e06195934041bfe696263f675102cc8a96
Salman Shaikh <salmans3724@gmail.com>
2025-06-04 20:58:46 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/codebysalmanshaikh/employee-task-tracker.git',
   plugins: [react() ,tailwindcss()],
 })
 // import { defineConfig } from 'vite'
